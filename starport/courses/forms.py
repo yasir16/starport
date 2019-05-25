b98d72ec2393663f85ce9f9f6d4730d73a44a916
Samuel Chan <samuel@outlook.sg>
2019-05-25 17:14:40 +0700

    better form validation

--- starport/courses/forms.py
+++ starport/courses/forms.py
@@ -2,12 +2,21 @@ import re
 from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
+
 # from django.core.validators import URLValidator
 
+
 def validate_repo_url(value):
-    reg = re.compile('^([a-zA-Z\d]{1}[-a-zA-Z\d]+)(/){1}([\-\w]+)$')
+    reg = re.compile("^([a-zA-Z\d]{1}[-a-zA-Z\d]+)(/){1}([\-\w]+)$")
+    message = _(f"{value} not a valid username/repo format.")
     if not reg.match(value):
-        raise ValidationError(f"{value} is not a valid url to a github repo")
+        raise ValidationError(message, code="invalid_format")
+        # raise ValidationError(f"{value} is not a valid url to a github repo")
+
 
 class AddtoQueueForm(forms.Form):
-    repo_url = forms.CharField(label="GitHub URL", max_length=100, validators=[validate_repo_url])
+    repo_url = forms.CharField(
+        label="GitHub URL", max_length=100, validators=[validate_repo_url]
+    )
+
--- starport/courses/views.py
+++ starport/courses/views.py
@@ -8,52 +8,47 @@ from .forms import AddtoQueueForm
 
 # Create your views here.
 def index(request):
-    coursesets = CourseSet.objects.order_by('-updated_on')[:5]
+    coursesets = CourseSet.objects.order_by("-updated_on")[:5]
     add_form = AddtoQueueForm()
-    template = loader.get_template('courses/index.html')
-    context = {
-        'coursesets': coursesets,
-        'add_form': add_form
-    }
-
+    template = loader.get_template("courses/index.html")
+    context = {"coursesets": coursesets, "add_form": add_form}
 
     return HttpResponse(template.render(context, request))
+
 
 def cs_detail(request, cs_id):
     try:
         courseset = CourseSet.objects.get(pk=cs_id)
     except CourseSet.DoesNotExist:
         raise Http404("The Course you're looking for is not found")
-    template = loader.get_template('courses/courseset.html')
-    context = {
-        'courseset': courseset,
-    }
+    template = loader.get_template("courses/courseset.html")
+    context = {"courseset": courseset}
     return HttpResponse(template.render(context, request))
 
+
 def c_detail(request, cs_id, c_id):
-    return HttpResponse(f'The detail page for Course {c_id}')
+    return HttpResponse(f"The detail page for Course {c_id}")
+
 
 def add_to_queue(request):
     form = AddtoQueueForm(request.POST)
     if form.is_valid():
-        existing_q = ApprovalQueue.objects.filter(repo_url=request.POST['repo_url'])
+        existing_q = ApprovalQueue.objects.filter(repo_url=request.POST["repo_url"])
         if existing_q:
-            return HttpResponse("Fail: This repo has already been submitted in the past.")
+            return HttpResponse(
+                "Fail: This repo has already been submitted in the past."
+            )
         else:
-            ApprovalQueue(repo_url=request.POST['repo_url']).save()
-            return HttpResponseRedirect(reverse('courses:index'))
+            ApprovalQueue(repo_url=request.POST["repo_url"]).save()
+            return HttpResponseRedirect(reverse("courses:index"))
     else:
-        return HttpResponse(f"{request.POST['repo_url']} is not in the specified username/repo format")
-
-# def add_to_queue(request):
-#     try:
-        
-#         aq = ApprovalQueue.objects.filter(repo_url=request.POST['repo_url'])
-#         if aq:
-#             raise HttpResponse("Fail: This repo has already been submitted in the past.")
-#         else:
-#             aq = ApprovalQueue(repo_url=request.POST['repo_url'])
-#             aq.save()
-#             return HttpResponseRedirect(reverse('courses:index'))
-#     except:
-#         raise HttpResponse("An error occured")
+        print(form.errors)
+        template = loader.get_template("courses/index.html")
+        coursesets = CourseSet.objects.order_by("-updated_on")[:5]
+        add_form = AddtoQueueForm()
+        context = {
+            "coursesets": coursesets,
+            "add_form": add_form,
+            "error_message": form.errors,
+        }
+        return HttpResponse(template.render(context, request))
