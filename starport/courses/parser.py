6d9005eb896b35159889d97197cf3db0c1640de5
onlyphantom <samuel@outlook.sg>
2019-07-05 19:44:52 +0700

    add regex on parser to capture choice

--- starport/courses/parser.py
+++ starport/courses/parser.py
@@ -3,7 +3,7 @@ import base64
 import re
 from bs4 import BeautifulSoup
 
-checkbox = re.compile("[\[]\s?[\]]")
+checkbox = re.compile("(?<=<li>[\[]\s[\]])(\s?)(.+)(?=</li>)")
 
 htmlcontent = "<ol>\n<li><p>Use the <code>engine.table_names()</code> to print the table names from <code>rcsample.db</code> located in the <code>/data</code> folder. How many tables were there?</p>\n<ul>\n<li>[ ] None</li>\n<li>[ ] 3</li>\n<li>[ ] 4</li>\n<li>[ ] 5</li>\n<li>2 points</li>\n</ul>\n</li>\n<li><p>Create an engine just like you did in (1) and execute a SQL query to count the number of rows within the <code>employee</code> table. How many rows are there?</p>\n<ul>\n<li>[ ] 18</li>\n<li>[ ] 28</li>\n<li>[ ] 38</li>\n<li>[ ] 48</li>\n<li>2 points</li>\n</ul>\n</li>"
 
@@ -17,15 +17,15 @@ def ExtractChoices(htmlcontent):
         for quest in ol.findAll("li", recursive=False):
             print(f"question: {quest.find('p')}")
             print("----")
-            print(f"choices: {quest.find('ul')}")
-            print("----")
-            x = list(quest.find("ul").li)
+            x = list(quest.find("ul").findAll("li"))
+            x = list(map(str, x))
     return questions, listitems, x
 
 
 q, l, x = ExtractChoices(htmlcontent)
-print(q)
-print(l)
+# Testing:
+#  Check if capturing the choice ignoring html tags
+#  re.search("(?<=<li>[\[]\s[\]])(\s?)(.+)(?=</li>)", x[2]).group(2)
 
 
 def readmeReader(repo_url):
